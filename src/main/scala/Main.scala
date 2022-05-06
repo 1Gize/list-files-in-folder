e1f27b060a95d21366779803a4391342e6824912
1Gize <grzolko@gmail.com>
2022-05-06 13:56:53 +0200

    fixed main so getListOfFiles take first argument as parameter

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,7 @@
 import java.io.File
 
 object Main extends App{
-  getListOfFiles("C:\\")
+  getListOfFiles(args.head)
   def getListOfFiles(dir: String) = {
     val d = new File(dir)
     if (d.exists && d.isDirectory) {
